d7677a6f42a398280c45ba8a2831f543a18b2710
Andrea <andreaxscala2017@libero.it>
2021-08-30 14:32:50 +0200

    .

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -4,22 +4,22 @@
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 800), "Pong");
+    sf::RenderWindow window(sf::VideoMode(1000, 800), "Pong", sf::Style::Titlebar | sf::Style::Close);
     sf::Event event;
     sf::RectangleShape paddle1(sf::Vector2f(20.f, 115.f));
     sf::RectangleShape paddle2(sf::Vector2f(20.f, 115.f));
-    sf::RectangleShape button(sf::Vector2f(100.f, 50.f));
+    sf::RectangleShape button(sf::Vector2f(100.f, 50.f)), buttonExit(sf::Vector2f(100.f, 50.f));
     sf::CircleShape ball(5.f);
     float ballSpeedx = 0,ballSpeedy = 0;
     sf::Color softblack(24, 26, 24);
     sf::Font font;
-    sf::Text mainText,buttonText,winner;
+    sf::Text mainText,buttonText,buttonExitText,winner,escForMainMenu;
     sf::Image icon;
     sf::SoundBuffer sb;
     sf::Sound pong;
-
+    int sign[2] = {-1, 1};
     srand(time(NULL));
-    bool gameStart = false;
+    bool gameStart = false,win = false;
 
     if (!icon.loadFromFile("../icon.png")) {
         MessageBox(NULL, (LPCWSTR)L"couldn't find window icon", NULL, MB_OK);
@@ -50,6 +50,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     winner.setFillColor(sf::Color::Red);
     winner.setPosition(250.f, 200.f);
 
+    escForMainMenu.setFont(font);
+    escForMainMenu.setString("press esc to go back to the main menu");
+    escForMainMenu.setCharacterSize(30);
+    escForMainMenu.setFillColor(sf::Color::Red);
+    escForMainMenu.setPosition(30.f, 300.f);
+
     buttonText.setFont(font);
     buttonText.setString("play");
     buttonText.setCharacterSize(30);
@@ -61,6 +67,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     button.setOutlineColor(sf::Color(40,40,40));
     button.setFillColor(sf::Color::Red);
     button.setPosition(450.f, 370.f);
+
+    buttonExit.setOutlineThickness(10.f);
+    buttonExit.setOutlineColor(sf::Color(40, 40, 40));
+    buttonExit.setFillColor(sf::Color::Red);
+    buttonExit.setPosition(450.f, 450.f);
+
+    buttonExitText.setFont(font);
+    buttonExitText.setString("exit");
+    buttonExitText.setCharacterSize(30);
+    buttonExitText.setFillColor(softblack);
+    buttonExitText.setPosition(450.f, 450.f);
+    buttonExitText.setLetterSpacing(0.3);
 
     ball.setFillColor(sf::Color::Yellow);
     ball.setPosition(495.f, 395.f);
@@ -76,35 +94,42 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
                 sf::Mouse::getPosition(window).x < 540.f && sf::Mouse::getPosition(window).y > 380.f && sf::Mouse::getPosition(window).y < 410)
                 gameStart = true;
             //close window
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed || sf::Mouse::isButtonPressed(sf::Mouse::Left) && sf::Mouse::getPosition(window).x > 460.f &&
+                sf::Mouse::getPosition(window).x < 540.f && sf::Mouse::getPosition(window).y > 460.f && sf::Mouse::getPosition(window).y < 490 && !gameStart && !win)
                 window.close();
             //pause while playing pressing esc
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
                 gameStart = false;
+                if (win) {
+                    win = false;
+                    ball.setPosition(495.f, 395.f);
+                    ballSpeedx = 0;
+                    ballSpeedy = 0;
+                    paddle1.setPosition(50.f, 342.5);
+                    paddle2.setPosition(930.f, 342.5);
+                }
+            }
         }
-        //paddles movement(outside of event listener to avoid delay while pressing keys for longer)
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && paddle1.getPosition().y < 685.f)
-            paddle1.move(0.f, 10.f);
+        if (gameStart) {
+            //paddles movement(outside of event listener to avoid delay while pressing keys for longer)
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && paddle1.getPosition().y < 685.f)
+                paddle1.move(0.f, 10.f);
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && paddle1.getPosition().y >= 10.f)
-            paddle1.move(0.f, -10.f);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && paddle1.getPosition().y >= 10.f)
+                paddle1.move(0.f, -10.f);
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && paddle2.getPosition().y < 685.f)
-            paddle2.move(0.f, 10.f);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && paddle2.getPosition().y < 685.f)
+                paddle2.move(0.f, 10.f);
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && paddle2.getPosition().y >= 10.f)
-            paddle2.move(0.f, -10.f);
-
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && paddle2.getPosition().y >= 10.f)
+                paddle2.move(0.f, -10.f);
+        }
         //start moving the ball
         if (gameStart && ballSpeedx == 0) {
             //set horizontal speed
-            ballSpeedx = rand() % 2;
-            if (ballSpeedx == 0)
-                ballSpeedx = -1;
+            ballSpeedx = sign[rand() % 2];
             //set vertical speed
-            ballSpeedy = rand() % 2;
-            if (ballSpeedy == 0)
-                ballSpeedy = -1;
+            ballSpeedy = sign[rand() % 2];
         }
         else if (gameStart && ballSpeedx != 0) {
             //bounce when hitting top or bottom wall
@@ -114,12 +139,19 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
             }
             else if (paddle1.getGlobalBounds().contains(ball.getPosition()) || paddle2.getGlobalBounds().contains(ball.getPosition())) {
                 ballSpeedx = -ballSpeedx;
+                ballSpeedy *= sign[rand() % 2];
                 pong.play();
             }
-            else if (ball.getGlobalBounds().left <= 0)
+            else if (ball.getGlobalBounds().left <= 0) {
+                gameStart = false;
+                win = true;
                 winner.setString("Player 2 wins");
-            else if (ball.getGlobalBounds().left + ball.getGlobalBounds().width >= 1000)
+            }
+            else if (ball.getGlobalBounds().left + ball.getGlobalBounds().width >= 1000) {
+                gameStart = false;
+                win = true;
                 winner.setString("Player 1 wins");
+            }
 
             ball.move(ballSpeedx, ballSpeedy);
             if (ballSpeedx < 0)
@@ -134,17 +166,20 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         }
 
         //"pause" menu
-        if (!gameStart) {
+        if (!gameStart && !win) {
             window.clear(softblack);
             window.draw(mainText);
             window.draw(button);
             window.draw(buttonText);
+            window.draw(buttonExit);
+            window.draw(buttonExitText);
             window.display();
         }
-        else if (!winner.getString().isEmpty()) {
+        else if (!winner.getString().isEmpty() && win) {
             //win screen
             window.clear(softblack);
             window.draw(winner);
+            window.draw(escForMainMenu);
             window.display();
         }
         else {//game stage
